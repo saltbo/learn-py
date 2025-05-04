0a18694a3ad078f17507412d12ac5adf4db06740
saltbo <saltbo@foxmail.com>
2025-05-04 15:50:53 +0800

    feat: learn the majorityElement
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- algo/arrays-medium/majorityElement.py
+++ algo/arrays-medium/majorityElement.py
@@ -1,7 +1,19 @@
 def majorityElement(array):
-    # Write your code here.
-    return -1
+    current = 0
+    count = 0
+    for num in array:
+        if count == 0:
+            current = num
+
+        if num != current:
+            count -= 1
+        else:
+            count += 1
+
+    return current
 
 
-array = [1, 2, 3, 2, 2, 1, 2]
+# array = [1, 2, 3, 2, 2, 1, 2]
+# array = [1, 1, 1, 1, 2, 2, 2, 2, 2]
+array = [2, 2, 1]
 print(majorityElement(array))
