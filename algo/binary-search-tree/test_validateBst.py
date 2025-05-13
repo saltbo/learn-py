6abe6f24c290a5f2160ac769fdfda7369ee6c725
saltbo <saltbo@foxmail.com>
2025-05-13 22:33:59 +0800

    feat: learn the validateBst
    Signed-off-by: saltbo <saltbo@foxmail.com>

---
+++ algo/binary-search-tree/test_validateBst.py
@@ -0,0 +1,27 @@
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
+import validateBst as program
+import unittest
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        self.assertEqual(program.validateBst(root), True)
