75cb87e5ac98f51030c01b10894710b350f56c0f
saltbo <saltbo@foxmail.com>
2025-05-18 17:16:40 +0800

    feat: learn the minHeap
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- algo/heaps/minHeap.py
+++ algo/heaps/minHeap.py
@@ -7,25 +7,67 @@ class MinHeap:
         self.heap = self.buildHeap(array)
 
     def buildHeap(self, array):
-        # Write your code here.
-        array[0]
+        self.heap = array
+        for currentIdx in range(self.parentIdx(len(array)-1), -1, -1):
+            self.siftDown(currentIdx)
 
-    def siftDown(self):
-        # Write your code here.
-        pass
+        return self.heap
 
-    def siftUp(self):
-        # Write your code here.
-        pass
+    def siftDown(self, currentIdx):
+        """
+        Adjust the element at the given index downward to ensure the subtree satisfies the min heap property
+        i.e., parent node value is less than or equal to its child node values
+        """
+        # Continue looping while the current node has at least one child
+        while self.hasLeftChild(currentIdx):
+            # Assume the left child is the smaller child
+            smallerChildIdx = self.leftChildIdx(currentIdx)
+
+            # If the right child exists and is smaller than the left child, update the smaller child to the right child
+            if (self.hasRightChild(currentIdx) and
+                    self.heap[self.rightChildIdx(currentIdx)] < self.heap[smallerChildIdx]):
+                smallerChildIdx = self.rightChildIdx(currentIdx)
+
+            # If the current node is already less than or equal to the smallest child, heap property is satisfied, exit loop
+            if self.heap[currentIdx] <= self.heap[smallerChildIdx]:
+                break
+
+            # Otherwise swap the current node with the smaller child, and continue adjusting downward
+            self.swap(currentIdx, smallerChildIdx)
+            currentIdx = smallerChildIdx
+
+    def siftUp(self, currentIdx):
+        while currentIdx > 0 and self.heap[currentIdx] < self.heap[self.parentIdx(currentIdx)]:
+            self.swap(currentIdx, self.parentIdx(currentIdx))
+            currentIdx = self.parentIdx(currentIdx)
 
     def peek(self):
-        # Write your code here.
-        pass
+        return self.heap[0]
 
     def remove(self):
-        # Write your code here.
-        pass
+        self.swap(0, len(self.heap)-1)
+        val = self.heap.pop()
+        self.siftDown(0)
+        return val
 
     def insert(self, value):
-        # Write your code here.
-        pass
+        self.heap.append(value)
+        self.siftUp(len(self.heap)-1)
+
+    def swap(self, currentIdx, targetIdx):
+        self.heap[currentIdx], self.heap[targetIdx] = self.heap[targetIdx], self.heap[currentIdx]
+
+    def leftChildIdx(self, idx):
+        return 2*idx+1
+
+    def rightChildIdx(self, idx):
+        return 2*idx+2
+
+    def parentIdx(self, idx):
+        return (idx-1)//2
+
+    def hasLeftChild(self, idx):
+        return self.leftChildIdx(idx) < len(self.heap)
+
+    def hasRightChild(self, idx):
+        return self.rightChildIdx(idx) < len(self.heap)
