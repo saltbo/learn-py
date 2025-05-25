2963c5c3c1c9dade3661adb6461a179c4dabab83
saltbo <saltbo@foxmail.com>
2025-05-25 09:39:53 +0800

    feat: learn the getYoungestCommonAncestor
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- algo/graphs/getYoungestCommonAncestor.py
+++ algo/graphs/getYoungestCommonAncestor.py
@@ -6,5 +6,32 @@ class AncestralTree:
 
 
 def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
-    # Write your code here.
-    pass
+    oneDepth = getDepth(topAncestor, descendantOne)
+    twoDepth = getDepth(topAncestor, descendantTwo)
+    print(oneDepth, twoDepth)
+
+    if oneDepth > twoDepth:
+        return findYCA(descendantOne, descendantTwo, oneDepth-twoDepth)
+    else:
+        return findYCA(descendantTwo, descendantOne, twoDepth - oneDepth)
+
+
+def findYCA(deeperOne, shalloerOne, diff):
+    while diff > 0:
+        deeperOne = deeperOne.ancestor
+        diff -= 1
+
+    while deeperOne != shalloerOne:
+        deeperOne = deeperOne.ancestor
+        shalloerOne = shalloerOne.ancestor
+
+    return deeperOne
+
+
+def getDepth(topAncestor, descendant):
+    depth = 0
+    while descendant != topAncestor:
+        depth += 1
+        descendant = descendant.ancestor
+
+    return depth
