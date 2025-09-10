ba5a5c932578d61bae0841ca32f57c58f40f8bd5
saltbo <saltbo@foxmail.com>
2025-09-10 10:26:21 -0400

    feat: learn the longestPalindromicSubstring
    Signed-off-by: saltbo <saltbo@foxmail.com>

--- algo/string-medium/longestPalindromicSubstring.py
+++ algo/string-medium/longestPalindromicSubstring.py
@@ -12,5 +12,28 @@ def isPalindromic(string, left, right):
     return string[left:right] == string[left:right][::-1]
 
 
+def longestPalindromicSubstring2(s):
+    longest = ""
+    for i in range(len(s)):
+        odd = expandAroundCenter(s, i, i)
+        even = expandAroundCenter(s, i, i+1)
+        if len(odd) > len(longest):
+            longest = odd
+        if len(even) > len(longest):
+            longest = even
+    return longest
+
+
+def expandAroundCenter(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+
+    print(left, right)
+    return s[left+1:right]
+
+
 string = "abaxyzzyxf"
-print(longestPalindromicSubstring(string))
+# print(expandAroundCenter(string, 5, 6))
+print(longestPalindromicSubstring2(string))
+# print(longestPalindromicSubstring(string))
